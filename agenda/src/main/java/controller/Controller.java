c6484b4a0290138f262f3b13ea0d3e5dc53af0da
MarcoMoura21 <mapmgi@hotmail.com>
2022-10-11 11:36:39 -0300

    JDBC - MySql - Camada MODEL(MVC) - remocao do teste conexao

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -28,7 +28,8 @@ public class Controller extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		dao.testeConexao();
+		// Teste de Conexão
+		// dao.testeConexao();
 	}
 	
 }
--- agenda/src/main/java/model/DAO.java
+++ agenda/src/main/java/model/DAO.java
@@ -13,25 +13,25 @@ public class DAO {
 	private String user = "root";
 	private String password = "1234";
 	// Método de conexão 
-	private Connection conectar() {
-		Connection con = null;
-		try {
-			Class.forName(driver);
-			con = DriverManager.getConnection(url, user, password);
-			return con;
-		} catch (Exception e) {
-			System.out.println(e);
-			return null;
+	 private Connection conectar() {
+		 Connection con = null;
+		 try {
+		     Class.forName(driver);
+			 con = DriverManager.getConnection(url, user, password);
+			 return con;
+		 } catch (Exception e) {
+			 System.out.println(e);
+			 return null;
 		}
 	}
 	// teste de conexão
-	public void testeConexao() {
-		try {
-			Connection con = conectar();
-			System.out.println(con);
-			con.close();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-	}
+	// public void testeConexao() {
+		// try {
+			// Connection con = conectar();
+			// System.out.println(con);
+			// con.close();
+		// } catch (Exception e) {
+			// System.out.println(e);
+		//}
+	// }
 }
