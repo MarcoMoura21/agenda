645648d5e1ff5eeabeb2abf65ef30829e7dbc941
MarcoMoura21 <mapmgi@hotmail.com>
2022-10-13 20:00:27 -0300

    CRUD READ - JDBC - ArrayList - Parte 1/2

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -1,11 +1,16 @@
 package controller;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import model.DAO;
 import model.JavaBeans;
 
@@ -36,8 +41,18 @@ public class Controller extends HttpServlet {
 	// Listar Contatos
 	protected void contatos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.sendRedirect("agenda.jsp");
+		
+		// Criando um objeto que irá receber um dados JavaBeans
+		ArrayList<JavaBeans> lista = dao.listarContatos();
+		// Teste de recebimento da lista
+		for (int i = 0; i < lista.size(); i++) {
+			System.out.println(lista.get(i).getIdcon());
+			System.out.println(lista.get(i).getNome());
+			System.out.println(lista.get(i).getFone());
+			System.out.println(lista.get(i).getEmail());
+		}			
 	}
+	
 
 	// Novo Contato
 	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
